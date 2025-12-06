09e8f6c18fecd34aafb191060a01f5b810e22fc4
walki <roger.john.walker@gmail.com>
2025-12-06 13:55:57 -0500

    module for leds

---
+++ lib/blinker/led.ex
@@ -0,0 +1,24 @@
+defmodule Blinker.Led do
+  alias Circuits.GPIO
+
+  def open(pin) do
+    message("Opening #{pin}")
+    {:ok, led} = GPIO.open(pin, :output)
+    led
+  end
+
+  def on(led) do
+    message("On: #{inspect(led)}")
+    GPIO.write(led, 1)
+  end
+
+  def off(led) do
+    message("Off: #{inspect(led)}")
+    GPIO.write(led, 0)
+  end
+
+  def message(message) do
+    # book warns of hardware unpredictability with IO.puts
+    IO.puts(message)
+  end
+end
