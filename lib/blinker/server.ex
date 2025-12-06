e9d4b59203eee37f3a6acf79b171ce71b2116595
walki <roger.john.walker@gmail.com>
2025-12-06 15:09:39 -0500

    add duration to server

--- lib/blinker/server.ex
+++ lib/blinker/server.ex
@@ -1,17 +1,25 @@
 defmodule Blinker.Server do
   alias Blinker.Led
 
-  defstruct [:led, :on, :ticker]
+  defstruct [:led, :on, :ticker, :duration]
   use GenServer
 
   @pin {"gpiochip0", 26}
 
   def new(opts) do
+    duration = opts[:duration] || 1000
+
     %__MODULE__{
       on: false,
       led: Led.open(opts[:pin] || @pin),
-      ticker: opts[:ticker] || (&wait/0)
+      ticker:
+        opts[:ticker] ||
+          fn ->
+            Blinker.Server.wait(duration)
+          end,
+      duration: duration
     }
+    |> dbg()
   end
 
   def start_link(opts \\ []) do
@@ -23,7 +31,7 @@ defmodule Blinker.Server do
     {:ok, new(opts)}
   end
 
-  def wait, do: Process.send_after(self(), :blink, 1000)
+  def wait(duration), do: Process.send_after(self(), :blink, duration)
 
   def handle_info(:blink, blinker) do
     blinker.ticker.()
